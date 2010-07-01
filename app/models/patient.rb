5fdfd9581429dcf8c7e3be48909b808660c38a67
thiagotnunes <tnunes@thoughtworks.com>
2010-07-01 11:10:27 -0300

    Evolution and evolution pictures are working properly

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -5,12 +5,13 @@ class Patient < ActiveRecord::Base
 	has_many :evolutions, :dependent => :destroy
 
 	accepts_nested_attributes_for :telephones, :allow_destroy => true
-	accepts_nested_attributes_for :evolutions, :allow_destroy => true
+	accepts_nested_attributes_for :evolutions, :allow_destroy => true, :reject_if => proc { |e| e['description'].empty? || e['attendance_date'].empty? }
 
 	validates_presence_of :name, :cpf, :rg, :sex, :birth_date, :health_insurance_id, :address, :address_number
 	validates_numericality_of :cpf, :rg
 	validates_length_of :cpf, :is => 11
 	validates_length_of :rg, :is => 10
-	validates_associated :health_insurance, :telephones #, :evolutions
+	validates_uniqueness_of :cpf, :rg
+	validates_associated :health_insurance, :telephones
 
 end
