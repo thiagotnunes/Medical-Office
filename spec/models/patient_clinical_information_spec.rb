8785de2c7d864813ec731c0d2c18ef386b525e58
thiagotnunes <tnunes@thoughtworks.com>
2010-08-03 14:52:32 -0300

    Complemented test for the patient clinical information

--- spec/models/patient_clinical_information_spec.rb
+++ spec/models/patient_clinical_information_spec.rb
@@ -3,6 +3,13 @@ require 'spec_helper'
 describe PatientClinicalInformation do
 
   it { should belong_to(:patient) }
+  
+  it { should validate_presence_of(:appointment_reason) }
+  
+  it { should ensure_length_of(:clazz).is_at_most(100) }
+  it { should ensure_length_of(:injury_side).is_at_most(10) }
+  it { should ensure_length_of(:injury_appraiser_physician).is_at_most(100) }
+  it { should ensure_length_of(:evolution_time).is_at_most(100) }
 
   it "should be valid patient clinical information with valid attributes" do
 		patient_clinical_information = Factory.build(:patient_clinical_information)
